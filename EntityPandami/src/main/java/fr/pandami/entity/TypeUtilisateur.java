900a3ea3db23981987209148b09defb214143dc2
Orane14 <oranedeganz@gmail.com>
2021-03-01 11:39:54 +0100

    adresse typeUtilisateur et preferenceService

--- EntityPandami/src/main/java/fr/pandami/entity/TypeUtilisateur.java
+++ EntityPandami/src/main/java/fr/pandami/entity/TypeUtilisateur.java
@@ -2,20 +2,90 @@ package fr.pandami.entity;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table (name = "TypeUtilisateur")
 public class TypeUtilisateur implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
 	@Id
 	@GeneratedValue (strategy = GenerationType.IDENTITY)
+
 	private Integer id;
 
+
 	private String libelle;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getLibelle() {
+		return libelle;
+	}
+
+	public void setLibelle(String libelle) {
+		this.libelle = libelle;
+	}
+
+	public TypeUtilisateur(Integer id, String libelle) {
+		super();
+		this.id = id;
+		this.libelle = libelle;
+	}
+
+	public TypeUtilisateur() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((libelle == null) ? 0 : libelle.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		TypeUtilisateur other = (TypeUtilisateur) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (libelle == null) {
+			if (other.libelle != null)
+				return false;
+		} else if (!libelle.equals(other.libelle))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "TypeUtilisateur [id=" + id + ", libelle=" + libelle + "]";
+	}
+	
+	
 
 }
